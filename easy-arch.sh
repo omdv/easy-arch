6e0fde3ec9e307d4c0fe2e02ecb06afcd0be1e38
Tommaso Chiti <chitilivorno@gmail.com>
2021-12-03 10:17:18 +0100

    removing grub specific entries.

--- easy-arch.sh
+++ easy-arch.sh
@@ -364,7 +364,7 @@ EOF
 
 # Enabling various services.
 print "Enabling Reflector, automatic snapshots, BTRFS scrubbing and systemd-oomd."
-for service in reflector.timer snapper-timeline.timer snapper-cleanup.timer btrfs-scrub@-.timer btrfs-scrub@home.timer btrfs-scrub@var-log.timer btrfs-scrub@\\x2esnapshots.timer grub-btrfs.path systemd-oomd
+for service in reflector.timer snapper-timeline.timer snapper-cleanup.timer btrfs-scrub@-.timer btrfs-scrub@home.timer btrfs-scrub@var-log.timer btrfs-scrub@\\x2esnapshots.timer systemd-oomd
 do
     systemctl enable $service --root=/mnt &>/dev/null
 done
